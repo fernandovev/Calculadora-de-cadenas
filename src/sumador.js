5d1b4f8d12f17594ebba959511826e7bf5c9182f
fernandovev <gualterbelmonte@gmail.com>
2026-05-27 22:05:54 -0400

    feat: aceptar multiples delimitadores

--- src/sumador.js
+++ src/sumador.js
@@ -3,26 +3,45 @@ function sumar(cadena) {
     return 0;
   }
 
-  let textoNumeros = cadena;
-  const delimitadores = [",", "-"];
-
-  if (cadena.startsWith("//")) {
-    const coincidencia = cadena.match(/^\/\/\[([^\]]+)\]\s*(.*)$/);
-    const delimitadorPersonalizado = coincidencia[1];
-
-    textoNumeros = coincidencia[2];
-    delimitadores.push(delimitadorPersonalizado);
-  }
+  const { textoNumeros, delimitadores } = obtenerConfiguracion(cadena);
 
   const separador = new RegExp(
-    delimitadores.map((delimitador) => escaparRegex(delimitador)).join("|")
+    delimitadores
+      .map((delimitador) => escaparRegex(delimitador))
+      .join("|")
   );
 
   return textoNumeros
     .split(separador)
     .map((numero) => Number(numero.trim()))
+    .filter((numero) => !Number.isNaN(numero))
     .filter((numero) => numero <= 1000)
     .reduce((suma, numero) => suma + numero, 0);
+}
+
+function obtenerConfiguracion(cadena) {
+  const delimitadoresBase = [",", "-"];
+
+  if (!cadena.startsWith("//")) {
+    return {
+      textoNumeros: cadena,
+      delimitadores: delimitadoresBase,
+    };
+  }
+
+  const coincidencia = cadena.match(/^\/\/((?:\[[^\]]+\])+)\s*(.*)$/);
+
+  const seccionDelimitadores = coincidencia[1];
+  const textoNumeros = coincidencia[2];
+
+  const delimitadoresPersonalizados = [
+    ...seccionDelimitadores.matchAll(/\[([^\]]+)\]/g),
+  ].map((coincidencia) => coincidencia[1]);
+
+  return {
+    textoNumeros,
+    delimitadores: [...delimitadoresBase, ...delimitadoresPersonalizados],
+  };
 }
 
 function escaparRegex(texto) {
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -6,34 +6,38 @@ describe("Calculadora de cadenas", () => {
   });
 
   it("devuelve el mismo numero cuando recibe un solo numero", () => {
-  expect(sumar("2")).toEqual(2);
+    expect(sumar("2")).toEqual(2);
   });
 
   it("suma dos numeros separados por coma", () => {
-  expect(sumar("1,2")).toEqual(3);
+    expect(sumar("1,2")).toEqual(3);
   });
 
   it("suma varios numeros separados por coma", () => {
-  expect(sumar("1,2,3")).toEqual(6);
+    expect(sumar("1,2,3")).toEqual(6);
   });
 
   it("acepta guion como separador", () => {
-  expect(sumar("1-2,3")).toEqual(6);
+    expect(sumar("1-2,3")).toEqual(6);
   });
 
   it("acepta un delimitador personalizado", () => {
-  expect(sumar("//[;] 6;7;4")).toEqual(17);
+    expect(sumar("//[;] 6;7;4")).toEqual(17);
   });
 
   it("combina delimitador personalizado con coma y guion", () => {
-  expect(sumar("//[;] 6,3-2;1")).toEqual(12);
+    expect(sumar("//[;] 6,3-2;1")).toEqual(12);
   });
 
   it("ignora numeros mayores a 1000", () => {
-  expect(sumar("2,1001")).toEqual(2);
+    expect(sumar("2,1001")).toEqual(2);
   });
 
   it("acepta delimitadores de cualquier longitud", () => {
-  expect(sumar("//[***] 1***2***3")).toEqual(6);
+    expect(sumar("//[***] 1***2***3")).toEqual(6);
+  });
+
+  it("acepta multiples delimitadores personalizados", () => {
+    expect(sumar("//[*][%] 1*2%3,7-9")).toEqual(22);
   });
 });
